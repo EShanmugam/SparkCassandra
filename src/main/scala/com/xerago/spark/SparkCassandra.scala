9166ea7419768e81663be98bf7cac0be0d08b922
EShanmugam <42334070+EShanmugam@users.noreply.github.com>
2018-08-13 11:49:45 +0530

    Add files via upload
    Source file added

---
+++ src/main/scala/com/xerago/spark/SparkCassandra.scala
@@ -0,0 +1,29 @@
+package com.xerago.spark
+
+import org.apache.spark.{SparkConf, SparkContext}
+import com.datastax.spark.connector._
+import com.datastax.spark.connector.cql.CassandraConnector
+import org.apache.spark
+import org.apache.spark.sql.cassandra
+/*import org.apache.spark.sql.SparkSession*/
+
+object SparkCassandra extends App {
+  case class Albums(title: String, country: String, performer:String, quality:String,status:String,year:Int)
+
+
+  override def main(args: Array[String]): Unit = {
+
+    val conf = new SparkConf(true)
+      .set("spark.cassandra.connection.host","10.50.255.247")
+      .set("spark.cassandra.auth.username", "root")
+      .set("spark.cassandra.auth.password", "xerago@999")
+      .setAppName("CassandraSpark").setMaster("local[*]")
+      .set("spark.cassandra.connection.port", "9042")
+
+    val sc = new SparkContext(conf)
+    val rdd = sc.cassandraTable("cvm", "customers")
+    val file_collect=rdd.collect()
+    file_collect.map(println(_))
+
+  }
+}
